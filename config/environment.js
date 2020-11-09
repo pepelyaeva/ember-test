93e935be435c5c4188bc7a89e23bf6e3ac68b85a
Pepelyaeva Kseniya <kpepelyaeva@ics.perm.ru>
2020-11-09 13:53:24 +0500

    Fix

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'ember-test',
     environment,
-    rootURL: '/',
+    rootURL: '/ember-test',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
